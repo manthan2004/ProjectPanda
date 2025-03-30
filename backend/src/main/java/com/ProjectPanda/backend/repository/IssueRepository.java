69022e422d7cab575c369baaee6ccb6aeefa50da
manthan2004 <bhatmanthan04@gmail.com>
2025-03-30 16:28:06 +0530

    created controller for issue

---
+++ backend/src/main/java/com/ProjectPanda/backend/repository/IssueRepository.java
@@ -0,0 +1,11 @@
+package com.ProjectPanda.backend.repository;
+
+import com.ProjectPanda.backend.modal.Issue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IssueRepository extends JpaRepository<Issue,Long> {
+
+    public List<Issue> findByProjectID(Long id);
+}
--- backend/src/main/java/com/ProjectPanda/backend/service/IssueService.java
+++ backend/src/main/java/com/ProjectPanda/backend/service/IssueService.java
@@ -2,6 +2,7 @@ package com.ProjectPanda.backend.service;
 
 
 import com.ProjectPanda.backend.modal.Issue;
+import com.ProjectPanda.backend.modal.User;
 import com.ProjectPanda.backend.request.IssueRequest;
 
 import java.util.List;
@@ -11,13 +12,13 @@ import java.util.Optional;
 
 public interface IssueService {
 
-    Optional<Issue> getIssueById(Long issueId) throws Exception;
+    Issue getIssueById(Long issueId) throws Exception;
 
     List<Issue> getIssueByProjectId(Long projectId) throws Exception;
 
-    Issue createIssue(IssueRequest issue, Long userid) throws Exception;
+    Issue createIssue(IssueRequest issue, User user) throws Exception;
 
-    String deleteIssue(Long issueId,Long userid) throws Exception;
+    void deleteIssue(Long issueId,Long userid) throws Exception;
 
     Issue addUserToIssue(Long issueId, Long userId) throws Exception;
 
